a046d75d3dc3e7a89b0af2ae2f6670b002578b18
AutomationAdithya <padithya560@gmail.com>
2018-07-07 09:33:47 +1000

    learning

---
+++ src/main/java/GITlearning/GITlearning/App.java
@@ -0,0 +1,13 @@
+package GITlearning.GITlearning;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
